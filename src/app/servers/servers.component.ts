83758a7b327b0f75d4174ef4a8279832b4fb84c9
Jet Balagtas <jetbalagtas@gmail.com>
2019-03-01 17:12:57 -0500

    using component selectors as html selectors/attributes (commented out)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   template: `
     <p>The Server Component</p>
     <p>The Server Component</p>`,
