bdc8104cde5d38602e9fbe0d9d8cf77fa7123bd4
Jet Balagtas <jetbalagtas@gmail.com>
2019-03-11 21:28:18 -0400

    output a list with the ngFor built-in directive

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => {
@@ -27,6 +28,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.servers.push(this.serverName);
     this.serverCreated = true;
   }
 
